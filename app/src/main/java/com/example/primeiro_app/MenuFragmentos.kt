ee86b4ac8eaa9a15d954407bd07ddc98e70b00e3
makzs <erikmakiak@gmail.com>
2024-09-21 19:00:41 -0300

    adicionando tela login e tela menu

--- app/src/main/java/com/example/primeiro_app/MenuFragmentos.kt
+++ app/src/main/java/com/example/primeiro_app/MenuFragmentos.kt
@@ -1,36 +1,20 @@
 package com.example.primeiro_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnLogin : Button
-
+class MenuFragmentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu_fragmentos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        btnLogin = findViewById(R.id.btnLogin)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, MenuFragmentos::class.java)
-
-            startActivity(intent)
-
-        }
     }
-
-
 }
