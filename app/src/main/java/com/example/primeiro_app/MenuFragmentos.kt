0f486a565e4eedf5ed8ba923762c03f0afa4b227
hugolp1 <hugolourencoprado@gmail.com>
2024-09-23 11:26:58 -0300

    Finalizada tela de perfil

--- app/src/main/java/com/example/primeiro_app/MenuFragmentos.kt
+++ app/src/main/java/com/example/primeiro_app/MenuFragmentos.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.primeiro_app.fragments.ListaMusicasFragment
@@ -27,13 +28,17 @@ class MenuFragmentos : AppCompatActivity() {
         if(savedInstanceState == null)
         {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView3, PerfilFragment())
+                .replace(R.id.fragmentContainerView3, ListaMusicasFragment())
                 .commit()
         }
 
         val bundle = intent.extras
         var nomePessoa = bundle?.getString("nomePessoa") ?: "sem nome"
+        val fragmento = PerfilFragment()
 
+        val bundle2 = bundleOf("nome" to nomePessoa)
+
+        fragmento.arguments = bundle2
 
 
         findViewById<Button>(R.id.btnMusicas).setOnClickListener {
@@ -44,10 +49,9 @@ class MenuFragmentos : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnPerfil).setOnClickListener {
-            val intent = Intent(this, PerfilFragment::class.java)
-            intent.putExtra("nome", nomePessoa)
+
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView3, PerfilFragment())
+                .replace(R.id.fragmentContainerView3, fragmento)
                 .addToBackStack(null)
                 .commit()
 
--- app/src/main/java/com/example/primeiro_app/fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/primeiro_app/fragments/PerfilFragment.kt
@@ -1,36 +1,25 @@
 package com.example.primeiro_app.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.example.primeiro_app.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PerfilFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PerfilFragment : Fragment() {
 
     private var txtNome : String? = null
     private lateinit var txtPerfil : TextView
-
+    private var image : Int? = null
+    private lateinit var btnLogout : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        txtNome = arguments?.getString("nomePessoa")
-
-
+        txtNome = arguments?.getString("nome")
     }
 
 
@@ -39,9 +28,20 @@ class PerfilFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_perfil, container, false)
+
+        btnLogout = view.findViewById<Button>(R.id.btnLogout)
+        txtPerfil = view.findViewById(R.id.txtPerfil)
+        txtPerfil.text = txtNome
+
+        btnLogout.setOnClickListener{
+            activity?.finish()
+        }
+
+        return view
     }
+
+
 
 
 }
